43208fd58aafc17fba04b641966927139fcbbd50
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-22 23:59:22 +0900

    [level 0] Title: 홀짝 구분하기, Time: 104.11 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        // /는 몫, %는 나머지
-        if ((a % 2) == 0) {
-            System.out.println(a + " is even");
+        int n = sc.nextInt();
+        
+        if (n % 2 == 0){
+            System.out.println(n + " is even");
         }
         else{
-            System.out.println(a + " is odd");
+            System.out.println(n + " is odd");
         }
     }
 }
