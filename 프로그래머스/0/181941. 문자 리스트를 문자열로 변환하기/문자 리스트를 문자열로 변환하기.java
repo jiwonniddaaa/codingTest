05b2ef9ecc90ad8589bdcb36d77aba7ab80ec645
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-24 23:10:43 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.66 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -1,10 +1,8 @@
 class Solution{
-    //String str은 단순 문자열 하나이지만, String[] arr은 문자열 배열을 받는다는 뜻
     public String solution(String[] arr){
-        //배열의 길이는 고정이라 괄호가 없음, 리스트/문자열은 괄호 있음
-        int a = arr.length;
+        int n = arr.length;
         String result = "";
-        for (int i = 0; i < a; i++){
+        for (int i = 0; i < n; i++){
             result += arr[i];
         }
         return result;
