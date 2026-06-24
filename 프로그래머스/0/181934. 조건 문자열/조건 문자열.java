4c2d92be7a086de78c4d49b62b95b4f073086bbd
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-24 23:46:23 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,21 +1,17 @@
-class Solution {
-    public int solution(String ineq, String eq, int n, int m) {
-        //String 비교는 .equals() 사용
-        //if나 else if에 안들어갈 수도 있으므로 return 기본값 지정
-        if (ineq.equals(">")) {
-            if (eq.equals("=")){
-                return (n >= m) ? 1 : 0;
-            }
-            else if(eq.equals("!")) {
-                return (n > m) ? 1 : 0;
-            }
+class Solution{
+    public int solution(String ineq, String eq, int n, int m){
+        if(ineq.equals(">")){
+           if(eq.equals("=")){
+               return (n >= m) ? 1 : 0;
+           } else if(eq.equals("!")){
+               return (n > m) ? 1 : 0;
+           }
         }
-        else if (ineq.equals("<")) {
-            if (eq.equals("=")){
+        else if(ineq.equals("<")){
+            if(eq.equals("=")){
                 return (n <= m) ? 1 : 0;
-            }
-            else if(eq.equals("!")) {
-                return (n < m) ? 1 : 0;
+            } else if(eq.equals("!")){
+                return (n < m) ? 1 : 0;   
             }
         }
         return 0;
