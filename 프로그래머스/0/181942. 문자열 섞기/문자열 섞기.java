95bbb5628b61689fe163aae7aebce15f8a3c1996
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-24 23:07:19 +0900

    [level 0] Title: 문자열 섞기, Time: 7.53 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,9 +1,8 @@
 class Solution {
-    //메서드에 타입 필수
     public String solution(String str1, String str2){
-        int a = str1.length();
+        int n = str1.length();
         String result = "";
-        for(int i = 0; i < a; i++){
+        for(int i = 0; i < n; i++){
             char p = str1.charAt(i);
             char q = str2.charAt(i);
             result = result + p + q;
