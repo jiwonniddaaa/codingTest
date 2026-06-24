ea6c68f4f639bca05a5afd231424658bff9744cc
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-24 23:26:39 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,5 +1,11 @@
-class Solution {
-    public int solution(int number, int n, int m) {
-        return (number % n == 0) & (number % m == 0) ? 1 : 0; 
+class Solution{
+    public int solution(int number, int n, int m){
+        if(number % n == 0){
+            if (number % m == 0){
+                return 1;
+            }
+            return 0;
+        }
+        return 0;
     }
 }
