46040d58a8b2fe1c14d978c9402d4f2903be265d
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-24 23:48:41 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
@@ -1,6 +1,5 @@
-class Solution {
-    // 삼항연산자와 if문은 복잡도 차이 없다, 구현 복잡 시 if문 사용 / 값 1개로 결정 시 삼항연산자
-    public int solution (int a, int b, boolean flag) {
+class Solution{
+    public int solution(int a, int b, boolean flag){
         return flag == true ? (a + b) : (a - b);
     }
 }
