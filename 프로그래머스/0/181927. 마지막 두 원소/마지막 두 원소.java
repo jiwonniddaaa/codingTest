2da4f37ca68c562a69668f43740b2a5ca55905e4
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-25 22:12:21 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,16 @@
+class Solution{
+    public int[] solution(int[] num_list){
+        int[] result = new int[num_list.length + 1];
+        for(int i = 0; i < num_list.length; i++){
+            result[i] = num_list[i];
+        }
+        
+        if(num_list[num_list.length - 1] > num_list[num_list.length - 2]){
+            result[num_list.length] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
+        }
+        else {
+            result[num_list.length] = num_list[num_list.length - 1] * 2;
+        }
+        return result;
+    }
+}
