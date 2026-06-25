c75ecbee54360942b932a4df5077b41f0a456d5b
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-25 18:36:06 +0900

    [level 0] Title: 코드 처리하기, Time: 170.60 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,31 +1,30 @@
-class Solution {
-    public String solution(String code) {
-        String answer = "";
+class Solution{
+    public String solution(String code){
         int mode = 0;
-        // 배열로 바꾸거나
-        char[] codeArr = code.toCharArray();
-        // 문자열에서 처리하거나 char current = code.charAt(idx);
-        for (int idx = 0; idx < codeArr.length; idx++) {
-            // 타입이 char이라 ''임
-            if (codeArr[idx] == '1') {
-                // ==는 비교연산자 =는 대입연산자
-                mode = 1 - mode;
-            }
-            else {
-                if (mode == 0) {
-                    if (idx % 2 == 0) {
-                        //자바의 string에는 append가 없음(StringBuilder 이런 곳에만 있음)
-                        answer += codeArr[idx];
+        String ret = "";
+        for(int i = 0; i < code.length(); i++){
+            char c = code.charAt(i);
+            if(mode == 0){
+                if(c == '1'){
+                    mode = 1;
+                }
+                else{
+                    if(i % 2 == 0){
+                        ret += c;
                     }
                 }
-                if (mode == 1) {
-                    if (idx % 2 != 0) {
-                        answer += codeArr[idx];
+            }
+            else if(mode == 1){
+                if(c == '1'){
+                    mode = 0;
+                }
+                else{
+                    if(i % 2 == 1){
+                        ret += c;
                     }
                 }
             }
         }
-        return answer.isEmpty()? "EMPTY" : answer;
+        return ret.isEmpty() ? "EMPTY" : ret; 
     }
 }
-
