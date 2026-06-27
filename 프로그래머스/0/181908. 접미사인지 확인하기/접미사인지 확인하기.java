6ba4d30b0296059f6801267ad36680e37aeae203
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-06-28 01:42:42 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,15 @@
+class Solution{
+    public int solution(String my_string, String is_suffix){
+        while(true){
+            if(is_suffix.length() > my_string.length()){
+                return 0;
+            }
+            for(int i = 0; i < is_suffix.length(); i++){
+                if(my_string.charAt(my_string.length() - is_suffix.length() + i) != is_suffix.charAt(i)){
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+}
