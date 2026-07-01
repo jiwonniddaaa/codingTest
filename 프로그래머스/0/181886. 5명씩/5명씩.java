dc1e281f0f1dd67db13645d2b5d78a7ad54d1e90
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-07-01 17:40:43 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Solution{
+    public String[] solution(String[] names){
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i < names.length; i += 5){
+            list.add(names[i]);
+        }
+        
+        String[] result = new String[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            result[i] = list.get(i);
+        }
+        
+        return result;
+    }
+}
