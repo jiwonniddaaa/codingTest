1f75b7d25dfddce17d55fd8765e1f27faa4bf217
Jiwon Kim <120795436+jiwonniddaaa@users.noreply.github.com>
2026-07-01 16:57:52 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.15 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Solution{
+    public int[] solution(int[] num_list, int n){
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = n; i < num_list.length; i++){
+            list.add(num_list[i]);
+        }
+        for(int i = 0; i < n; i++){
+            list.add(num_list[i]);
+        }
+        
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            result[i] = list.get(i);
+        }
+        
+        return result;
+    }
+}
